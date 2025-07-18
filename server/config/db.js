858967c0df6df94d69716067e47f9019ea757c5b
Akin Holo <holoakinseloyin@gmail.com>
2025-07-18 09:23:54 +0100

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
    try {
-      const conn = await mongoose.connect(process.env.MONGO_URI);
+      const conn = await mongoose.connect(process.env.MONGO_URI, {
+         useNewUrlParser: true,
+         useUnifieldTopology: true,
+         ssl: true,
+         tks: true,
+         serverSelectionTimeoutMS: 5000,
+      });
       console.log(`MongoDB connected: ${conn.connection.host}`);
    } catch (err) {
       console.error(err.message);
